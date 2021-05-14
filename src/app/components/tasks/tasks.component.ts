278c9fa0349a8bd150c8e78337fa6302f6bc439e
stan000 <stanmpakatig@gmail.com>
2021-05-14 18:53:01 +0200

    Added individual task components

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { TASKS } from 'src/app/mockTasks';
+import { Task } from 'src/app/Tasks';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
+  tasks: Task[] = TASKS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
