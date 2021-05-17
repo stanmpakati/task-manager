92276a339488dc096e28f12039acc96825b9a91d
stan000 <stanmpakatig@gmail.com>
2021-05-17 17:40:54 +0200

    delete funtionality added

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Observable } from 'rxjs';
 import { Task } from 'src/app/Tasks';
 
 @Component({
@@ -9,9 +10,14 @@ import { Task } from 'src/app/Tasks';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
+  @Output() onTaskDelete: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onDelete(task: Task) {
+    this.onTaskDelete.emit(task);
+  }
 }
